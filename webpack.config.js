c853dc669a94b8acd9b2da1bf01025e3dcc2a419
TonyG Mac <triplegdev@gmail.com>
2018-09-06 13:21:16 -0400

    Completed desktop and mobile header styles.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+	entry: "./app/assets/scripts/App.js",
+	output: {
+		path: path.resolve(__dirname, "./app/temp/scripts"),
+		filename: "App.js"
+	},
+	mode: "development",
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['env']
+				},
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	} 
+}
